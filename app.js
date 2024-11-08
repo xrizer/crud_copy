922ad949d408c7f55f2c39f4e1bed8a9d3b44c45
Aqshol Afifi <aqshol.afifi@tapera.go.id>
2024-11-08 18:02:10 +0700

    setup changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 app.use(bodyParser.json());
 let books = [
   { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
